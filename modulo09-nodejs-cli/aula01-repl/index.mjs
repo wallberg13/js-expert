35f1f72a987ebf4d4c08cacd47e362c2ea433495
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2025-10-05 15:39:52 -0300

    feat: add module 09 class 01 - repl

---
+++ modulo09-nodejs-cli/aula01-repl/index.mjs
@@ -0,0 +1,24 @@
+// curl "http://localhost:3000?salary=3000&discount=15"
+
+import http from "http";
+
+function netSalary({ discount, salary }) {
+  const percent = discount / 100;
+  const cost = salary * percent;
+  const result = salary - cost;
+
+  return result;
+}
+
+http
+  .createServer((req, res) => {
+    const url = req.url.replace("/", "");
+    const params = new URLSearchParams(url);
+    const data = Object.fromEntries(params);
+    const result = netSalary(data);
+    debugger;
+    res.end(`O seu salário final é: ${result}`);
+  })
+  .listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
